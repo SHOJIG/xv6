36cc7fdbb8883bfeac5506026d6537c61c6b6770
shojig <762056667@qq.com>
2025-04-23 09:51:44 +0800

    Changes to be committed:

--- kernel/printf.c
+++ kernel/printf.c
@@ -117,6 +117,7 @@ printf(char *fmt, ...)
 void
 panic(char *s)
 {
+  backtrace();
   pr.locking = 0;
   printf("panic: ");
   printf(s);
@@ -131,4 +132,15 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void backtrace() {
+  uint64 fp = r_fp();
+  int pagetop = PGROUNDUP(fp);
+  printf("backtrace:\n");
+  while(fp <= pagetop) {
+    uint64 ra = *(uint64*)(fp - 8);
+    printf("%p\n", ra);
+    fp = *(uint64*)(fp - 16);
+  }
 }
