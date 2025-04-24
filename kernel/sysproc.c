ac7135a199b6518b5def8d1486e1eacc5e7828d5
shojig <762056667@qq.com>
2025-04-24 17:16:51 +0800

    Changes to be committed:

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,14 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  struct proc* p = myproc();
+  // if(growproc(n) < 0)
+  //   return -1;
+  if(n > 0) {
+    p->sz += n;
+  }else if(p ->sz + n >= PGROUNDUP(p->trapframe -> sp)) {
+    p->sz = uvmdealloc(p->pagetable, addr, addr + n);
+  }else return -1;
   return addr;
 }
 
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,7 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
-
+#include "proc.h"
+#include "spinlock.h"
 /*
  * the kernel's page table.
  */
@@ -101,10 +102,14 @@ walkaddr(pagetable_t pagetable, uint64 va)
     return 0;
 
   pte = walk(pagetable, va, 0);
-  if(pte == 0)
-    return 0;
-  if((*pte & PTE_V) == 0)
-    return 0;
+  if(pte == 0 || (*pte & PTE_V) == 0){
+    if(uvmshouldallocate(va)) {
+      uvmlazyallocate(va);
+    }else {
+      return 0;
+    }
+  }
+  //pte = walk(pagetable, va, 0);
   if((*pte & PTE_U) == 0)
     return 0;
   pa = PTE2PA(*pte);
@@ -181,9 +186,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      // panic("uvmunmap: walk");
+      continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      // panic("uvmunmap: not mapped");
+      continue;
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -315,9 +322,11 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      // panic("uvmcopy: pte should exist");
+      continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      // panic("uvmcopy: page not present");
+      continue;
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
@@ -438,5 +447,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return 0;
   } else {
     return -1;
+  }
+}
+
+int uvmshouldallocate(uint64 va) {
+  pte_t* pte;
+  struct proc* p = myproc();
+  return (va < p->sz && va > p->trapframe->sp && ((pte = walk(p->pagetable, va, 0)) == 0 || ((*pte & PTE_V) == 0)));
+}
+
+void uvmlazyallocate(uint64 va) {
+  struct proc* p = myproc();
+  void* pa = kalloc();
+  if(pa == 0) {
+    p->killed = 1;
+  }
+  else {
+    memset(pa, 0, PGSIZE);
+    if(mappages(p->pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)pa, PTE_W|PTE_R|PTE_U) != 0) {
+      kfree(pa);
+      p->killed = 1;
+    }
   }
 }
