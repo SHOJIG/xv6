f5c21ad78e2b18e9432c073a1f84a64b201d1852
shojig <762056667@qq.com>
2025-05-12 19:02:51 +0800

    Changes to be committed:

--- kernel/defs.h
+++ kernel/defs.h
@@ -8,6 +8,7 @@ struct spinlock;
 struct sleeplock;
 struct stat;
 struct superblock;
+struct vma;
 
 // bio.c
 void            binit(void);
@@ -171,6 +172,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+void            vmaunmap(pagetable_t pagetable, uint64 va, uint64 nbytes, struct vma* v);
 
 // plic.c
 void            plicinit(void);
@@ -185,3 +187,6 @@ void            virtio_disk_intr(void);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
+
+struct vma*     findvma(struct proc* p, uint64 va);
+int             vmaalloc(uint64 va);
--- kernel/proc.h
+++ kernel/proc.h
@@ -1,4 +1,6 @@
 // Saved registers for kernel context switches.
+#include "spinlock.h"
+
 struct context {
   uint64 ra;
   uint64 sp;
@@ -82,6 +84,18 @@ struct trapframe {
 
 enum procstate { UNUSED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+#define NVMA 16
+
+struct vma {
+    int valid;              // 该虚拟内存区域是否已被映射
+    uint64 vastart;         // 该虚拟内存区域开始地址
+    uint64 sz;              // 该虚拟内存区域大小
+    struct file* f;         // 该虚拟内存区域映射的文件
+    int prot;               // 该虚拟内存区域权限
+    int flags;              // 标记映射内存的修改是否写回文件
+    uint64 offset;          // 映射文件的起点
+};
+
 // Per-process state
 struct proc {
   struct spinlock lock;
@@ -103,4 +117,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct vma vmas[NVMA];
 };
+
+
